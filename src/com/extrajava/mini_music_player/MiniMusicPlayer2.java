dc22d2302e152b4293c680d247cb04c5318f1dcd
Eugene <sirius-tyrin@yandex.ru>
2021-02-09 13:12:26 +0300

    Midi-плеер 1.2

---
+++ src/com/extrajava/mini_music_player/MiniMusicPlayer2.java
@@ -0,0 +1,57 @@
+package com.extrajava.mini_music_player;
+
+/**
+ * Класс Музыкальный плеер + сообщение в командной строке
+ * @autor EvgeniiTiurin
+ * @version 1.2
+ *
+ * В данной версии создается код упрощающий создание и добавление MIDI-событий
+ * Добавляем регистрацию и прослушивание событий. Вывод сообщения в командную строку
+ */
+
+import javax.sound.midi.*;
+
+public class MiniMusicPlayer2 implements ControllerEventListener {
+
+    public static void main(String[] args) {
+        MiniMusicPlayer2 mini = new MiniMusicPlayer2();
+        mini.go();
+    }
+
+    public void go() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this, eventsIWant);
+
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            for (int i = 5; i < 60; i+=4) {
+                track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(176,1,127,0,i));
+                track.add(makeEvent(128, 1, i, 100, i + 2));
+            }
+
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
+        } catch (Exception ex) { ex.printStackTrace(); }
+    }
+
+    public void controlChange(ShortMessage event) {
+        System.out.println("ля");
+    }
+
+    public MidiEvent makeEvent (int comd, int chan, int one, int two, int tick) {
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        } catch (Exception e) { }
+        return event;
+    }
+}
